30587caca74f9085f44c6f4db3afe7ae7866b3e0
Lapisu <dmitriiycmo@gmail.com>
2023-09-12 20:59:00 +0300

    I HATE JAVA

--- Test.java
+++ Test.java
@@ -1,4 +1,9 @@
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args){}
+
         
+
+    public static void I_HATE_JAVA(){
+        System.out.println("IHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVAIHATEJAVA");
+    }
 }
